c730c0d60ea6dcacb1fbef668aa9aadfdada8418
Rohit1502 <rohit.bhosale@neosofttech.com>
2019-10-14 13:48:12 +0530

    Initial commit

---
+++ src/main/java/com/neo/bookameetingroom/BookAMeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.neo.bookameetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAMeetingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAMeetingRoomApplication.class, args);
+	}
+
+}
